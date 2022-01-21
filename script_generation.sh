4ab0ee2e034e1ef63cd7172b8e2453157a9fec1c
BenWebBrowser <benjamin@verspeet.fr>
2022-01-21 09:53:24 +0100

    add grep

--- script_generation.sh
+++ script_generation.sh
@@ -1,5 +1,5 @@
 cd /home/x
 mkdir crontab_script 
 cd /home/x/massa/massa-client/
-echo $(/home/x/massa/target/release/massa-client wallet_info) > /home/x/crontab_script/adresses.txt
-chmod 777 /home/x/crontab_script/addresses.txt
+echo $(/home/x/massa/target/release/massa-client wallet_info | grep Address) > /home/x/crontab_script/adresses.txt
+chmod 777 /home/x/crontab_script/adresses.txt
