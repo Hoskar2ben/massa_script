6d6c40aa75f8a119ee3df4962328e2a1a248da99
BenWebBrowser <benjamin@verspeet.fr>
2022-01-21 09:59:33 +0100

    decoupage valeur

--- script_generation.sh
+++ script_generation.sh
@@ -1,5 +1,5 @@
 cd /home/x
 mkdir crontab_script 
 cd /home/x/massa/massa-client/
-echo $(/home/x/massa/target/release/massa-client wallet_info | grep Address) > /home/x/crontab_script/adresses.txt
+echo $(/home/x/massa/target/release/massa-client wallet_info | grep Address | awk -F " " '{print $3}') > /home/x/crontab_script/adresses.txt
 chmod 777 /home/x/crontab_script/adresses.txt
